59739dd950294937d837884ebd7e27df1a3d8ca0
litmanmb@gmail.com <litmanmb@gmail.com>
2025-02-18 10:13:18 -0300

    improved logic

--- src/Banco.java
+++ src/Banco.java
@@ -1,24 +1,37 @@
 import java.util.List;
 
 public class Banco {
+    private String nome;
+    private List<Conta> contas;
 
-	private String nome;
-	private List<Conta> contas;
+    public Banco(String nome, List<Conta> contas) {
+        this.nome = nome;
+        this.contas = contas;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public List<Conta> getContas() {
-		return contas;
-	}
+    public List<Conta> getContas() {
+        return contas;
+    }
 
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
 
+    // Método para realizar a operação de sacar em todas as contas de um cliente
+    public void realizarSaque(Cliente cliente, double valor) {
+        for (Conta conta : contas) {
+            if (conta.getCliente().equals(cliente)) {
+                conta.sacar(valor);
+                break;
+            }
+        }
+    }
 }
--- src/Conta.java
+++ src/Conta.java
@@ -1,52 +1,50 @@
-
 public abstract class Conta implements IConta {
-	
-	private static final int AGENCIA_PADRAO = 1;
-	private static int SEQUENCIAL = 1;
+    private Cliente cliente;
+    private double saldo;
 
-	protected int agencia;
-	protected int numero;
-	protected double saldo;
-	protected Cliente cliente;
+    public Conta(Cliente cliente) {
+        this.cliente = cliente;
+        this.saldo = 0.0;
+    }
 
-	public Conta(Cliente cliente) {
-		this.agencia = Conta.AGENCIA_PADRAO;
-		this.numero = SEQUENCIAL++;
-		this.cliente = cliente;
-	}
+    public Cliente getCliente() {
+        return cliente;
+    }
 
-	@Override
-	public void sacar(double valor) {
-		saldo -= valor;
-	}
+    public double getSaldo() {
+        return saldo;
+    }
 
-	@Override
-	public void depositar(double valor) {
-		saldo += valor;
-	}
+    // Métodos comuns
+    protected void imprimirInfosComuns() {
+        System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("Saldo: " + saldo);
+    }
 
-	@Override
-	public void transferir(double valor, IConta contaDestino) {
-		this.sacar(valor);
-		contaDestino.depositar(valor);
-	}
+    @Override
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque de " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
 
-	public int getAgencia() {
-		return agencia;
-	}
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito de " + valor + " realizado com sucesso.");
+    }
 
-	public int getNumero() {
-		return numero;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	protected void imprimirInfosComuns() {
-		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
-		System.out.println(String.format("Agencia: %d", this.agencia));
-		System.out.println(String.format("Numero: %d", this.numero));
-		System.out.println(String.format("Saldo: %.2f", this.saldo));
-	}
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            contaDestino.depositar(valor);
+            System.out.println("Transferência de " + valor + " realizada com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
 }
